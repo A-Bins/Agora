acda59eb19d91f0f8a82222f61cc569d0333f894
A-Bins <yyangsuk33@naver.com>
2021-06-09 19:07:19 +0900

    GoldRush Schduler ( Timer ) 구조 완성. 코드 작업만 남음

--- src/main/kotlin/com/bins/agora/structure/objects/GoldRush.kt
+++ src/main/kotlin/com/bins/agora/structure/objects/GoldRush.kt
@@ -4,6 +4,7 @@ import com.bins.agora.Agora.Companion.goldRush
 import com.bins.agora.Agora.Companion.ins
 import com.bins.ruins.utilities.Receiver.bb
 import org.bukkit.Bukkit
+import org.bukkit.Sound
 import org.bukkit.entity.Player
 import kotlin.collections.ArrayList
 
@@ -34,8 +35,18 @@ class GoldRush {
         }
     private fun timer(timer: Int, isCancelled: (Int) -> Boolean, title: (Int) -> String, end: () -> Any) {
         ins.server.scheduler.runTaskLater(ins, Runnable {
-            if(isCancelled(waiters.size)) return@Runnable
-            if(timer == 10 || (5 downTo 1).contains(timer)) goldRush.waiters.forEach { it.sendTitle("","시작까지 ${title(timer)}${timer}초 남았습니다..", 20, 20, 20)}
+            if(isCancelled(waiters.size)){
+                goldRush.waiters.forEach {
+                    it.playSound(it.location, Sound.ENTITY_VILLAGER_NO, 1F, 1F)
+                    it.sendTitle("","§c시작까지 필요한 인원이 부족합니다!", 20, 20, 20)
+                }
+                return@Runnable
+            }
+            if(timer == 10 || (5 downTo 1).contains(timer)) goldRush.waiters.forEach {
+
+                it.playSound(it.location, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1F, 2F)
+                it.sendTitle("","시작까지 ${title(timer)}${timer}초 남았습니다..", 20, 20, 20)
+            }
             if(timer != 0) timer(timer-1, isCancelled, title, end)
             else end()
         }, 20)
